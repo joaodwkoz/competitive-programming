a38a837169668581803f499c3fe0ac014dda6dca
João Pedro <fjoaopedro1302@gmail.com>
2026-01-27 21:40:44 -0300

    feat: exercício 601 do neps resolvido

---
+++ adhoc/neps/601_PecaPerdida.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    int s = 0;
+    for (int i = 1; i < n; i++) {
+        int x;
+        cin >> x;
+        s += x;
+    }
+
+    cout << (n * (n + 1) / 2) - s << endl;
+    
+    return 0;
+}

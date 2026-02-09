3f1949ed552dc97b37d4a1ec370baa055fff8bff
João Pedro <fjoaopedro1302@gmail.com>
2026-02-08 21:54:20 -0300

    feat: exercício 1978 do neps resolvido

---
+++ simulacao/neps/1978_ConviteDeFesta.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    vector<int> arr(n + 1);
+    for (int i = 1; i <= n; i++) arr[i] = i;
+
+    int m;
+    cin >> m;
+    for (int i = 1; i <= m; i++) {
+        int x, c = 1;
+        cin >> x;
+        for (int i = 1; i <= n; i++) {
+            if (arr[i] != -1) {
+                if (!(arr[i] % x)) {
+                    arr[i] = -1;
+                } else {
+                    arr[i] = c++;
+                }
+            }
+        }
+    }
+
+    for (int i = 1; i <= n; i++) {
+        if (arr[i] != -1) {
+            cout << i << endl;
+        }
+    }
+
+    return 0;
+}

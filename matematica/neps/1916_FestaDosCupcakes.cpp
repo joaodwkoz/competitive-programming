dfc83331de4d32fb53a086a53db7f45d052cf7b3
João Pedro <fjoaopedro1302@gmail.com>
2026-02-26 19:24:09 -0300

    feat: exercício 1916 do neps resolvido

---
+++ matematica/neps/1916_FestaDosCupcakes.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int r, s;
+    cin >> r >> s;
+
+    cout << (r * 8 + s * 3) - 28 << endl;
+
+    return 0;
+}

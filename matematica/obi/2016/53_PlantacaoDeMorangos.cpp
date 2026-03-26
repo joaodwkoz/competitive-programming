4d2b5285e8a2e71c54ceebf9328ecfe293532b25
João Pedro <fjoaopedro1302@gmail.com>
2026-03-26 18:45:59 -0300

    feat: exercício 53 do neps(obi - 2016) resolvido

---
+++ matematica/obi/2016/53_PlantacaoDeMorangos.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << max(a * b, c * d) << endl;
+
+    return 0;
+}

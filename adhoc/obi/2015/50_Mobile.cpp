e0da8263a3b408516f43542d4f887df21d356ed6
João Pedro <fjoaopedro1302@gmail.com>
2026-04-04 19:42:17 -0300

    feat: exercício 50 do neps(obi - 2015) resolvido

---
+++ adhoc/obi/2015/50_Mobile.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+
+    cout << (a == b + c + d && d == b + c && b == c ? "S" : "N") << endl;
+
+    return 0;
+}

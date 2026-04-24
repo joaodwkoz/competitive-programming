bf7914b0abccdd4fd9151b9124c96aebc6a250c7
João Pedro Ferreira <fjoaopedro1302@gmail.com>
2026-04-24 20:38:36 -0300

    feat: exercício 491 do neps resolvido

---
+++ estruturas-de-dados/conjunto/neps/491_Loteria.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+
+int main(){	
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int x;
+    set<int> st;
+    
+	for (int i = 1; i <= 6; i++) {
+        cin >> x;
+        st.insert(x);
+    }
+
+    int ans = 0;
+    for (int i = 1; i <= 6; i++) {
+        cin >> x;
+        ans += (int)(st.count(x));
+    }
+
+    if (ans >= 3) {
+        string terms[4] = {"terno", "quadra", "quina", "sena"};
+        cout << terms[ans - 3] << endl;
+    } else {
+        cout << "azar" << endl;
+    }
+
+    return 0;
+}

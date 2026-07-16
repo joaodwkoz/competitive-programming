ff28a9fc07ee4432c333eb29f228979b77ac172d
João Pedro <fjoaopedro1302@gmail.com>
2026-07-16 12:47:22 -0300

    feat: exercício 2888 do neps resolvido

---
+++ matematica/neps/2888_Cadeado.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n;
+    cin >> n;
+
+    int ans = 0, x, y;
+    for (int i = 1; i <= n; i++) {
+        cin >> x >> y;
+        int distH = ((y - x) + 10) % 10;
+        int distAH = ((x - y) + 10) % 10;
+        ans += min(distH, distAH);
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}

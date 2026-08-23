aaf6fd9ec247e84de2425b58ab829923f2a2a883
João Pedro <fjoaopedro1302@gmail.com>
2026-08-23 12:50:01 -0300

    style: código padronizado

--- matematica/neps/530_OMarNaoEstaParaPeixe.cpp
+++ matematica/neps/530_OMarNaoEstaParaPeixe.cpp
@@ -6,8 +6,7 @@ const int MAX_C = 110;
 
 bool arr[MAX_C][MAX_C];
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
@@ -15,23 +14,18 @@ int main()
     cin >> n;
 
     int x1, x2, y1, y2;
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++) {
         cin >> x1 >> x2 >> y1 >> y2;
-        for (int j = x1; j < x2; j++)
-        {
-            for (int k = y1; k < y2; k++)
-            {
+        for (int j = x1; j < x2; j++) {
+            for (int k = y1; k < y2; k++) {
                 arr[j][k] = true;
             }
         }
     }
 
     int ans = 0;
-    for (int i = 1; i < MAX_C; i++)
-    {
-        for (int j = 1; j < MAX_C; j++)
-        {
+    for (int i = 1; i < MAX_C; i++) {
+        for (int j = 1; j < MAX_C; j++) {
             ans += (int)(arr[i][j]);
         }
     }
